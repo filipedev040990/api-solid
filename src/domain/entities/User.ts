85437f31645572664bc65cd88dba63aac6fc8e9e
Filipe Siqueira <filipedev040990@gmail.com>
2022-07-22 11:53:02 -0300

    Criando entidade User

---
+++ src/domain/entities/User.ts
@@ -0,0 +1,19 @@
+import { uuid } from 'uuidv4';
+export class User {
+    public readonly id: string;
+    public name: string;
+    public email: string;
+    public password: string;
+    public created_at: Date;
+    public updated_at: Date;
+
+    constructor(props: Omit<User, 'id'| 'created_at'| 'updated_at'>, id?: string) {
+        Object.assign(this, props);
+
+        if(!id) {
+            this.id = uuid();
+        }
+        this.created_at = new Date();
+        this.updated_at = new Date();
+    }
+}
